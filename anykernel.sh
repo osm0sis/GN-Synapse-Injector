e484651683984b0e8a82b17382bbf97decbb4c7e
osm0sis <osm0sis@outlook.com>
2017-11-22 13:32:06 -0400

    anykernel: add ramdisk_compression shell variable, see e5f4262c00ebd98515b1dd7ab18bf483eb484a78

--- anykernel.sh
+++ anykernel.sh
@@ -17,6 +17,7 @@ device.name3=toroplus
 # shell variables
 block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
 is_slot_device=0;
+ramdisk_compression=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
